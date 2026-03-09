26fb43c7d4f0a2fce3abbf6fedf01a9ba58887ff
PedroDaou <pedro.mpd@puccampinas.edu.br>
2026-03-08 21:24:17 -0300

    feat: add receivable type risk weight to scoring (BOLETO, PIX, CONTRACT, etc.

--- src/domain/receivable/ReceivableType.java
+++ src/domain/receivable/ReceivableType.java
@@ -1,10 +1,21 @@
 package domain.receivable;
 
+import java.math.BigDecimal;
+
 public enum ReceivableType {
-    CREDIT_CARD,
-    DEBIT_CARD,
-    BOLETO,
-    PIX,
-    SUBSCRIPTION,
-    CONTRACT;
+    CONTRACT(new BigDecimal("1.00")),
+    SUBSCRIPTION(new BigDecimal("0.95")),
+    CREDIT_CARD(new BigDecimal("0.90")),
+    DEBIT_CARD(new BigDecimal("0.88")),
+    PIX(new BigDecimal("0.85")),
+    BOLETO(new BigDecimal("0.75"));
+
+    private final BigDecimal weight;
+
+    ReceivableType(BigDecimal weight) {
+        this.weight = weight;
+    }
+    public BigDecimal getWeight() {
+        return weight;
+    }
 }
